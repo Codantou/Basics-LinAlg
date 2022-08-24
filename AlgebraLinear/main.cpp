82d58cb19ff26feffa9f9b4bd8a8150e1cdc7a61
Cherif Ahmad Seye <cherifseye2018@gmail.com>
2022-08-23 21:35:42 -0400

    Initial Commit

---
+++ AlgebraLinear/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AlgebraLinear
+//
+//  Created by Castekc U on 2022-08-23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
